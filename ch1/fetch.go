2617861205fabeb148af6c54efb02cfae4426a6c
shiqi-lu <traumlou@gmail.com>
2022-01-15 22:18:19 +0800

    ch1 fetch.go

---
+++ ch1/fetch.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "os"
+)
+
+func main() {
+    for _, url := range os.Args[1:] {
+        resp, err := http.Get(url)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+            os.Exit(1)
+        }
+        // 返回结果存在响应结构 resp 里面
+        // 其中 resp 的 Body 域包含服务器端响应的一个可读取数据流
+        //  读取完响应结果需关闭 Body 数据流避免资源泄露
+        b, err := ioutil.ReadAll(resp.Body)
+        resp.Body.Close()
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+            os.Exit(1)
+        }
+        fmt.Printf("%s", b)
+    }
+}
+
